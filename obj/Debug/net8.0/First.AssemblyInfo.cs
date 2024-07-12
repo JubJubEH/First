c7ce7f5fbd88edfb777ce5c4a23907ea2121c923
JubJubEH <nathanhanne115@gmail.com>
2024-07-12 12:48:14 -0400

    new

--- obj/Debug/net8.0/First.AssemblyInfo.cs
+++ obj/Debug/net8.0/First.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("First")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b8cca7f59ef585cc1da01ba25c9fc9711fe47e1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3f953e95ee255b6831e9f9baaca9259322e7dc0")]
 [assembly: System.Reflection.AssemblyProductAttribute("First")]
 [assembly: System.Reflection.AssemblyTitleAttribute("First")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
